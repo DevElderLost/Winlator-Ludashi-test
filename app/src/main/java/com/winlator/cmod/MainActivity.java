a8ddf9c0b09fa657b2bfdd77d4b66653010eef4c
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-04-01 13:18:46 +0800

    Refactor MainActivity imports and add context
    Refactor MainActivity by adding context import and organizing imports.

--- app/src/main/java/com/winlator/cmod/MainActivity.java
+++ app/src/main/java/com/winlator/cmod/MainActivity.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.content.Context;
 
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;

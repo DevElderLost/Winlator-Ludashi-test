3728fbfee974be82d117903cecd2ecb570eae0b8
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-04-17 12:53:14 +0800

    Add files via upload

--- app/src/main/java/com/winlator/cmod/renderer/material/WindowMaterial.java
+++ app/src/main/java/com/winlator/cmod/renderer/material/WindowMaterial.java
@@ -1,47 +1,37 @@
-package com.winlator.renderer.material;
+package com.winlator.cmod.renderer.material;
 
 public class WindowMaterial extends ShaderMaterial {
-    public final Uniforms uniforms = new Uniforms();
-
-    public static class Uniforms {
-        public final Uniform xform = new Uniform("xform");
-        public final Uniform viewSize = new Uniform("viewSize");
-        public final Uniform texture = new Uniform("texture");
-        public final Uniform noAlpha = new Uniform("noAlpha");
-        public final Uniform flipY = new Uniform("flipY");
+    public WindowMaterial() {
+        setUniformNames("xform", "viewSize", "texture");
     }
 
     @Override
     protected String getVertexShader() {
-        return String.join("\n",
-            "uniform float xform[6];",
-            "uniform vec2 viewSize;",
-            "uniform bool flipY;",
+        return
+            "uniform float xform[6];\n" +
+            "uniform vec2 viewSize;\n" +
+            "attribute vec2 position;\n" +
+            "varying vec2 vUV;\n" +
 
-            "attribute vec2 position;",
-            "varying vec2 vUV;",
-
-            "void main() {",
-                "vUV = vec2(position.x, flipY ? (1.0 - position.y) : position.y);",
-                "vec2 transformedPos = applyXForm(position, xform);",
-                "gl_Position = vec4(2.0 * transformedPos.x / viewSize.x - 1.0, 1.0 - 2.0 * transformedPos.y / viewSize.y, 0.0, 1.0);",
+            "void main() {\n" +
+                "vUV = position;\n" +
+                "vec2 transformedPos = applyXForm(position, xform);\n" +
+                "gl_Position = vec4(2.0 * transformedPos.x / viewSize.x - 1.0, 1.0 - 2.0 * transformedPos.y / viewSize.y, 0.0, 1.0);\n" +
             "}"
-        );
+        ;
     }
 
     @Override
     protected String getFragmentShader() {
-        return String.join("\n",
-            "precision mediump float;",
+        return
+            "precision mediump float;\n" +
 
-            "uniform sampler2D texture;",
-            "uniform float noAlpha;",
-            "varying vec2 vUV;",
+            "uniform sampler2D texture;\n" +
+            "varying vec2 vUV;\n" +
 
-            "void main() {",
-                "vec4 texelColor = texture2D(texture, vUV);",
-                "gl_FragColor = vec4(texelColor.rgb, max(texelColor.a, noAlpha));",
+            "void main() {\n" +
+                "gl_FragColor = vec4(texture2D(texture, vUV).rgb, 1.0);\n" +
             "}"
-        );
+        ;
     }
 }

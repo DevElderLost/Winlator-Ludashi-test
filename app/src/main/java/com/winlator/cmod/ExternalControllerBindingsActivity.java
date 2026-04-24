a2b3b742ca1281cff34a2e98e853d476acbeb431
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-04-24 15:28:56 +0800

    Refactor joystick input processing and binding updates

--- app/src/main/java/com/winlator/cmod/ExternalControllerBindingsActivity.java
+++ app/src/main/java/com/winlator/cmod/ExternalControllerBindingsActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.InputDevice;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
